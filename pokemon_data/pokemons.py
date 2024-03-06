2251c9cb8261e06c7e8604dd20b8b7a7c023c684
Raymond Zeng <shilei.zeng98@outlook.com>
2024-03-05 19:37:00 -0500

    Added img_url to Pokemon class

--- pokemon_data/pokemons.py
+++ pokemon_data/pokemons.py
@@ -12,13 +12,14 @@ for i in range(151):
   pokemon_url_description_list.append(base_url_description + str(i+1))
 
 class Pokemon:
-  def __init__(self, name, types, description, height, weight):
+  def __init__(self, name, types, description, height, weight, image_url):
     self.name = name  # name of the pokemon, in pokemon[forms][0][name]
     self.types = types  # list of types, in pokemon[types][0][type][name] and pokemon[types][1][type][name]
     self.description = description  # description of the pokemon, in pokemon[flavor_text_entries][0][flavor_text]
     self.height = height  # height of the pokemon, in pokemon[height]
     self.weight = weight  # weight of the pokemon, in pokemon[weight]
-
+    self.image_url = image_url  # image of the pokemon, in pokemon[sprites][other][official-artwork][front_default]
+    
 # Get the data from the APIs for each pokemon and its description and store it as Pokemon object in a list
 pokemon_data_list = []    
   
