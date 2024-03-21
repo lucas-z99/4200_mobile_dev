ae9c15d440fd7db23000284f6afbcbc436c4faae
Raymond Zeng <shilei.zeng98@outlook.com>
2024-03-21 15:09:20 -0400

    Found out intent is where bug is.

--- app/src/main/java/com/example/a4200_group_project/MainActivity.java
+++ app/src/main/java/com/example/a4200_group_project/MainActivity.java
@@ -11,33 +11,31 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    int pokemonId = 1;  // Keeps track of current pokemon, default 1
+    int pokemonId = 5;  // Keeps track of current pokemon, default 1
     String name;
     String type1;
     String type2;
     String description = "failed test";
-    int height;
-    int weight;
+    String height;
+    String weight;
     String image_url;
+    String jsonResponse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         fetchAndSetPokemonData(pokemonId);
-
-
-//        //TEST
-//        PokeServer_Example p = new PokeServer_Example();
-//        p.example();
     }
 
     public void fetchAndSetPokemonData(int id){
         PokeServer.getPokemon(id, this::onSuccess, this::onFail);
+
     }
 
     // For keeping track of pokemon data across pages.
     public void updateSharedPreferences(){
+        System.out.println("UpdateSharedPreferences");
         SharedPreferences sp = getPreferences(MODE_PRIVATE);
         SharedPreferences.Editor spe = sp.edit();
         spe.putString("id", String.valueOf(pokemonId));
@@ -45,8 +43,8 @@ public class MainActivity extends AppCompatActivity {
         spe.putString("description", description);
         spe.putString("type1", type1);
         spe.putString("type2", type2);
-        spe.putString("height", String.valueOf(height));
-        spe.putString("weight", String.valueOf(weight));
+        spe.putString("height", height);
+        spe.putString("weight", weight);
         spe.putString("image_url", image_url);
         spe.apply();
     }
@@ -55,35 +53,46 @@ public class MainActivity extends AppCompatActivity {
     void onSuccess(String content) {
         System.out.println("  ----------   success  ----------\n");
         System.out.println(content);
-        // Convert json string to json object.
+        jsonResponse = content;
+        updatePokeData();
+        updateSharedPreferences();
+        updateViews();
+
+//      Testing before having main page button for this feature.
+        //TODO: Fix this
+        Intent intent = new Intent(MainActivity.this, PokemonInfo.class);
+        startActivity(intent);
+    }
+
+    // Update pokemon views
+    public void updateViews(){
+        System.out.println("updateViews");
+        System.out.println(pokemonId);
+        System.out.println(name);
+        System.out.println(description);
+        System.out.println(image_url);
+    }
+
+    public void updatePokeData() {
         JSONObject jsonObj = null;
         try {
-            jsonObj = new JSONObject(content);
+            System.out.println("UpdatePokeData");
+            jsonObj = new JSONObject(jsonResponse);
             pokemonId = (int) jsonObj.get("id");
             name = (String) jsonObj.get("name");
             type1 = (String) jsonObj.get("type0");
             type2 = (String) jsonObj.get("type1");
             description = (String) jsonObj.get("description");
-            height = (int) jsonObj.get("height");
-            weight = (int) jsonObj.get("weight");
+            height = String.valueOf(jsonObj.get("height"));
+            weight = String.valueOf(jsonObj.get("weight"));
             image_url = (String) jsonObj.get("image_url");
-            updateSharedPreferences();
-            updateViews();
-            // Testing before having main page button for this feature.
-            Intent intent = new Intent(this, PokemonInfo.class);
-            startActivity(intent);
+            System.out.println("UpdatePokeData Finished");
         } catch (JSONException e) {
-            System.out.println("Failed Jsonify json string.");
+            System.out.println("UpdatePokeData Failed");
             throw new RuntimeException(e);
         }
 
     }
-
-    // Update pokemon views
-    public void updateViews(){
-
-    }
-
     void onFail(Exception e) {
         System.out.println("  ----------   fail   ----------\n");
         e.printStackTrace();
