9b5909e19055fca4512a9822ae3fac9d292ba5b2
LateNightNuggies <137971669+Asphel01@users.noreply.github.com>
2024-03-20 20:33:29 -0400

    Addition of the activity class for the info page

---
+++ app/src/main/java/com/example/a4200_group_project/PokemonInfo.java
@@ -0,0 +1,15 @@
+package com.example.a4200project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PokemonInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pokemon_info);
+    }
+    
+}
