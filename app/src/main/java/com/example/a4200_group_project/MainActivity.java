4c841b41fa60d643b0b87993d061c2c1f2522c2a
Raymond Zeng <shilei.zeng98@outlook.com>
2024-03-28 10:18:33 -0400

    Hooked up to MapActivities

--- app/src/main/java/com/example/a4200_group_project/MainActivity.java
+++ app/src/main/java/com/example/a4200_group_project/MainActivity.java
@@ -106,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
         mapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(PokedexIntroActivity.this, MapsActivity.class);
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(intent);
             }
         });
